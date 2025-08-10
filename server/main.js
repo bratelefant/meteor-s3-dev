8ac28161fb21a2f27e8378529079b374327c05aa
bratelefant <christian.wahle+github@gmail.com>
2025-08-10 10:25:40 +0200

    refactor: format webhookBaseUrl assignment for better readability

--- server/main.js
+++ server/main.js
@@ -11,7 +11,9 @@ const s3 = new MeteorS3({
     process.env.AWS_SECRET_ACCESS_KEY || Meteor.settings?.aws?.secretAccessKey,
   name: "publicFiles",
   endpoint: "http://localhost:4566", // Optional, this defaults to the AWS S3 endpoint
-  webhookBaseUrl: Meteor.isDevelopment ? "http://" + process.env.LOCAL_IP + ":3000" : undefined, // Optional, this defaults to the AWS S3 webhook endpoint
+  webhookBaseUrl: Meteor.isDevelopment
+    ? "http://" + process.env.LOCAL_IP + ":3000"
+    : undefined, // Optional, this defaults to the AWS S3 webhook endpoint
   region: "eu-central-1", // Optional, this defaults to 'eu-central-1'
   verbose: true, // Optional, this defaults to false
   skipPermissionChecks: false, // Optional, this defaults to false
